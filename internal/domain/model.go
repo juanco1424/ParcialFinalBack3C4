4f5974ba9787dc55e82c8a8061f91cd401052de0
Laura Lopez <lorojas.laure@gmail.com>
2023-09-16 20:30:09 -0500

    change data type from id string to int

--- internal/domain/model.go
+++ internal/domain/model.go
@@ -1,7 +1,7 @@
 package domain
 
 type Patient struct {
-	Id            string `json:"id"`
+	Id            int `json:"id"`
 	Name          string `json:"name" binding:"required"`
 	LastName      string `json:"last_name" binding:"required"`
 	Address       string `json:"address" binding:"required"`
